d1190607be94b3752064815d5cc61cb51ac3ba97
kodai <kodai030513@gmail.com>
2024-06-06 23:11:45 +0900

    part2完了

--- Part 1 - Displaying Data/MonkeyFinder/Services/MonkeyService.cs
+++ Part 1 - Displaying Data/MonkeyFinder/Services/MonkeyService.cs
@@ -1,5 +1,35 @@
-﻿namespace MonkeyFinder.Services;
+﻿using System.Net.Http.Json;
+
+namespace MonkeyFinder.Services;
 
 public class MonkeyService
 {
+    List<Monkey> monkeyList = new List<Monkey>();
+
+    HttpClient httpClient;
+    public MonkeyService()
+    {
+        this.httpClient = new HttpClient();
+    }
+    public async Task<List<Monkey>> GetMonkeys()
+    {
+
+        if(monkeyList.Count > 0)
+            return monkeyList;
+
+        //online
+        var response = await httpClient.GetAsync("https://www.montemagno.com/monkeys.json");
+        if(response.IsSuccessStatusCode)
+        {
+            monkeyList = await response.Content.ReadFromJsonAsync(MonkeyContext.Default.ListMonkey);
+        }
+
+        //offline
+        using var stream = await FileSystem.OpenAppPackageFileAsync("monkeydata.json");
+        using var reader = new StreamReader(stream);
+        var contents = await reader.ReadToEndAsync();
+        monkeyList = JsonSerializer.Deserialize(contents, MonkeyContext.Default.ListMonkey);
+
+        return monkeyList;
+    }
 }
